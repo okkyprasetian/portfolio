bb40d240a1536387d913b415abee4b2142e33cd1
okkyprasetian <okkyprasetian@gmail.com>
2021-12-24 10:17:38 +0700

    create send message with from functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,23 @@
+import emailjs from 'emailjs-com'
 import { FiSend } from 'react-icons/fi'
 
 function Form() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('gmail', 'service_vwuczw9', e.target, 'user_HbTla61k0n7L8rmDGzpY6')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            })
+        e.target.reset()
+    }
+
     return (
         <form className="form">
-            <div className="form-container">
+            <div className="form-container" onSubmit={sendEmail}>
                 <input type="text" className="form-input form-name" placeholder="Name" name="name" />
                 <input type="text" className="form-input form-email" placeholder="Email" name="email" />
                 <input type="text" className="form-input form-subject" placeholder="Subject" name="subject" />
